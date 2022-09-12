a45509933e0c22fc325e1da6878d5e26da0d1dc9
Matthew Fung <mfung@codal.com>
2022-09-12 15:50:00 +0100

    Add ex 2.5

--- part2/course-information/src/App.js
+++ part2/course-information/src/App.js
@@ -1,30 +1,4 @@
-const Header = ({ course }) => <h1>{course}</h1>
-
-const Total = ({ sum }) => <p>Number of exercises {sum}</p>
-
-const Part = ({ part }) => (
-  <p>
-    {part.name} {part.exercises}
-  </p>
-)
-
-const Content = ({ parts }) => (
-  <>
-    {parts.map(part => <Part key={part.id} part={part} />)}
-  </>
-)
-
-const Course = ({ course }) => {
-  let total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
-
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total sum={total} />
-    </div>
-  )
-}
+import Course from './components/course';
 
 const App = () => {
   const courses = [
