6e2bc07f58fa08fbbafa1ecd5dbddf89d533e41a
Matthew Fung <mfung@codal.com>
2022-09-12 12:08:23 +0100

    Add ex 1.7->1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,5 +1,36 @@
 import { useState } from 'react'
 
+const Statistics = ({ good, neutral, bad }) => {
+  let total = good + neutral + bad,
+    average = ((good * 1) + (bad * -1)) / total,
+    percentagePositive = (good / total) * 100;
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <th>Good</th><td>{good}</td>
+        </tr>
+        <tr>
+          <th>Netural</th><td>{neutral}</td>
+        </tr>
+        <tr>
+          <th>Bad</th><td>{bad}</td>
+        </tr>
+        <tr>
+          <th>All</th><td>{total}</td>
+        </tr>
+        <tr>
+          <th>Average</th><td>{average}</td>
+        </tr>
+        <tr>
+          <th>Postive</th><td>{percentagePositive}</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0)
@@ -8,7 +39,15 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <button onClick={setGood}>Good</button>
+      <button onClick={setNeutral}>Netural</button>
+      <button onClick={setBad}>Bad</button>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+      />
     </div>
   )
 }
