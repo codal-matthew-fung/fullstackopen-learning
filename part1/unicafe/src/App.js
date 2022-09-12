0e36ef2cbcd084651c45e31f36c6fb428afe482d
Matthew Fung <mfung@codal.com>
2022-09-12 12:13:15 +0100

    Add ex 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -36,18 +36,18 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  let statsComponent;
+  if (good + neutral + bad > 0) {
+    statsComponent = <Statistics good={good} neutral={neutral} bad={bad} />
+  }
   return (
     <div>
       <h1>Give Feedback</h1>
       <button onClick={setGood}>Good</button>
       <button onClick={setNeutral}>Netural</button>
       <button onClick={setBad}>Bad</button>
-      <Statistics
-        good={good}
-        neutral={neutral}
-        bad={bad}
-      />
+      {statsComponent}
+
     </div>
   )
 }
